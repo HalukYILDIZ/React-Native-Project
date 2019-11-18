6a3bc7ac1c06d3950f17fc8dff586d2773e68161
HalukYILDIZ <halukyildiz4000@gmail.com>
2019-11-18 16:57:37 +0300

    deneme

--- App.js
+++ App.js
@@ -121,7 +121,7 @@ class HomeScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
+        <Text>Ana Sayfa</Text>
         <Button
           title="Go to Details"
           onPress={() => this.props.navigation.navigate('Details')}
